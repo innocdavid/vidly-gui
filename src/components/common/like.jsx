8f8bf0eaaaf2ae42e599413f0e635d3a3c0b03ff
Banja David Lotigo <innocdavid@gmail.com>
2022-12-05 17:18:22 +0300

    Update: Feature Increment, Refactor code

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from "react";
 
 const Like = props => {
   let classes = "fa fa-heart";
-  if (!props.liked ) classes += "-o";
+  if (!props.liked) classes += "-o";
   return (
-    <i 
-      onClick={props.onClick} 
-      className={classes} 
-      aria-hidden="true"
+    <i
+      onClick={props.onClick}
       style={{ cursor: "pointer" }}
-    >
-    </i>
+      className={classes}
+      aria-hidden="true"
+    />
   );
-
-}
+};
 
 export default Like;
--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 
-const ListGroup = props => {
-  const { items, valueProperty, textProperty, onItemSelect, selectedItem } = props;
-
+const ListGroup = ({
+  items,
+  textProperty,
+  valueProperty,
+  selectedItem,
+  onItemSelect
+}) => {
   return (
     <ul className="list-group">
       {items.map(item => (
-        <li 
+        <li
           onClick={() => onItemSelect(item)}
-          key={item[valueProperty]} 
-          className={item === selectedItem ? "list-group-item active" : "list-group-item" }
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
         >
           {item[textProperty]}
         </li>
       ))}
     </ul>
   );
-}
+};
 
 ListGroup.defaultProps = {
-  valueProperty: '_id',
-  textProperty: 'name'
-}
+  textProperty: "name",
+  valueProperty: "_id"
+};
 
 export default ListGroup;
