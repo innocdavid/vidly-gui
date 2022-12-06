8be5d41252f34c8cc3b21bb2b797c16fc1228223
Banja David Lotigo <innocdavid@gmail.com>
2022-12-06 10:59:00 +0300

    HANDLE ROUTES - Add customers component - Add notFound component - Add rentals component - Add movieForm component - Handle routes to different pages - Redirect to page not found if route name not correct

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import Movies from './components/movies';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
+import Movies from './components/movies';
+import Customers from './components/customers';
+import Rental from './components/rental';
+import NotFound from './components/notFound';
 
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/rental" element={<Rental />} />
+            <Route path="/not-found" element={<NotFound />} />
+            <Route path="/" element={<Navigate to="/movies" />} />
+            <Route path="*" element={<Navigate to="/not-found" />} />
+          </Routes>
+        </BrowserRouter>
       </main>
     )
   }
